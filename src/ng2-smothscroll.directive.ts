55d94b9ba1b7ff61c5cee57ead68476a2e9e35c4
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-07-05 15:52:25 -0500

    removing the service and making a directive with the logic

---
+++ src/ng2-smothscroll.directive.ts
@@ -0,0 +1,10 @@
+import { Directive, Input } from '@angular/core';
+
+import { WINDOW } from './window.service';
+
+@Directive({
+  selector: '[smothscroll]'
+})
+export class DirectiveNameDirective {
+  
+}
