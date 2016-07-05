106a45f131c64a9f855f0ac6819e686187da28b9
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-07-05 16:39:49 -0500

    Trying to integrate the ng2-scroll script in the example directory

--- example/src/app/components/home/home.ts
+++ example/src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
+import {SmothScrollDirective} from 'ng2-smothscroll';
 
 @Component({
   selector: 'home',
   pipes: [],
   providers: [],
-  directives: [],
+  directives: [SmothScrollDirective],
   styleUrls: ['./home.css'],
   templateUrl: './home.html'
 })
